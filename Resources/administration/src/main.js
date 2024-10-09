0467d5bb43ed159d84372556d7fb63ad233e50b3
Suraj Raya <surraya58@gmail.com>
2024-10-09 15:15:49 +0200

    initial

---
+++ Resources/administration/src/main.js
@@ -0,0 +1,5 @@
+import './module/sw-customer-detail';
+
+Shopware.Component.override('sw-customer-detail', {
+    template: './sw-customer-detail.html.twig'
+});
---
+++ Resources/administration/src/module/sw-customer-detail/index.js
@@ -0,0 +1,31 @@
+import template from './sw-customer-detail.html.twig';
+
+Shopware.Component.override('sw-customer-detail', {
+    template,
+
+    inject: ['repositoryFactory'],
+
+    data() {
+        return {
+            customer: null,  // Initialize the customer object
+            customerRepository: null
+        };
+    },
+
+    created() {
+        this.customerRepository = this.repositoryFactory.create('customer'); // Initialize the customer repository
+        this.loadCustomerData();
+    },
+
+    methods: {
+        loadCustomerData() {
+            const customerId = this.$route.params.id; // Get customer ID from route
+
+            this.customerRepository.get(customerId, Shopware.Context.api).then(customer => {
+                this.customer = customer; // Assign the fetched customer data
+            }).catch(error => {
+                console.error("Error loading customer data", error);
+            });
+        }
+    }
+});
